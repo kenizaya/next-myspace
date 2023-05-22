dc531eb30d119d143e2567616204e8fc16500ed0
kenizaya <harveylstark@gmail.com>
2023-05-23 00:05:29 +0530

    Implement Blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
-const page = () => {
-  return <div>page</div>
+const Blog = async () => {
+  const posts = await fetch('http://localhost:3000/api/content').then((res) =>
+    res.json()
+  )
+
+  return (
+    <div>
+      <h1>Welcome to our Blog</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.slug}>
+            <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-export default page
+export default Blog

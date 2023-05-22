35ad0145cc09d70096fc1b63ecb4f9601d2daed7
kenizaya <harveylstark@gmail.com>
2023-05-22 23:35:05 +0530

    Add NavMenu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import NavMenu from './NavMenu'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <NavMenu />
+        {children}
+      </body>
     </html>
   )
 }
